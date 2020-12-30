604d6103386c326d89fba368d6585e7aa3fcd302
jehunseo <31786318+jehunseo@users.noreply.github.com>
2020-12-30 23:07:42 +0900

    Create 10951.c

---
+++ Baekjoon/10951.c
@@ -0,0 +1,12 @@
+//link : https://www.acmicpc.net/problem/10951
+#include <stdio.h>
+
+int main(void){
+   int A, B;
+    
+    while(~scanf("%d %d", &A, &B)){
+        printf("%d\n", A+B);
+    }
+    
+    return 0;
+}
