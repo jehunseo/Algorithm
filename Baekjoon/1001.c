a6501961855b04453d88fe28363caef6b9e1312e
jehunseo <31786318+jehunseo@users.noreply.github.com>
2020-12-30 22:34:16 +0900

    Create 1001.c

---
+++ Baekjoon/1001.c
@@ -0,0 +1,11 @@
+//link : https://www.acmicpc.net/problem/1001
+#include <stdio.h>
+
+int main(void){
+    int var1, var2;
+    
+    scanf("%d %d", &var1, &var2);
+    printf("%d", var1-var2);
+    
+    return 0;
+}
