f3a34b593ecba8101e0eec4270259a542218f2d5
jehunseo <31786318+jehunseo@users.noreply.github.com>
2020-12-30 22:49:37 +0900

    Create 2558.c

---
+++ Baekjoon/2558.c
@@ -0,0 +1,11 @@
+//link : https://www.acmicpc.net/problem/2558
+#include <stdio.h>
+
+int main(void){
+    int var1, var2;
+    
+    scanf("%d", &var1);
+    scanf("%d", &var2);
+    
+    printf("%d", (var1)+(var2));
+}
