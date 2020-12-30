1f4024436227d5f0af6501c2c5550e6c4a5c1227
jehunseo <31786318+jehunseo@users.noreply.github.com>
2020-12-30 23:08:18 +0900

    Create 10998.c

---
+++ Baekjoon/10998.c
@@ -0,0 +1,9 @@
+//link : https://www.acmicpc.net/problem/10998
+#include <stdio.h>
+
+int main(void){
+    int var1, var2;
+    scanf("%d %d", &var1, &var2);
+    printf("%d", var1*var2);
+    return 0;
+}
