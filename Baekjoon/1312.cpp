bcba23a7da0b1f217210007167714409e95b32c2
jehunseo <31786318+jehunseo@users.noreply.github.com>
2020-12-30 22:39:21 +0900

    Create 1312.cpp

---
+++ Baekjoon/1312.cpp
@@ -0,0 +1,24 @@
+//link : https://www.acmicpc.net/problem/1312
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a, b, n;
+	int res = 0;
+	
+	cin >> a >> b >> n;
+	if( a % b == 0){
+		cout << 0;
+		return 0;
+	}
+	
+	if( a > b ) a = a % b;
+	
+	for(int i=0;i<n;i++){
+		a *= 10;
+		res = (int)(a / b);	
+		a = a % b;
+	}
+	cout << res;
+}
