5e895dfee60122b279bb602d7c833fdbd9d2ad0e
jehunseo <31786318+jehunseo@users.noreply.github.com>
2020-12-30 23:06:21 +0900

    Create 10869.c

---
+++ Baekjoon/10869.c
@@ -0,0 +1,16 @@
+//link : https://www.acmicpc.net/problem/10869
+#include <stdio.h>
+
+int main(void){
+    int var1, var2;
+    
+    scanf("%d %d", &var1, &var2);
+    
+    printf("%d\n", var1+var2);
+    printf("%d\n", var1-var2);
+    printf("%d\n", var1*var2);
+    printf("%d\n", var1/var2);
+    printf("%d\n", var1%var2);
+    
+    return 0;
+}
