71544a9db1107bd2400bae48483605d2cd96435e
jehunseo <31786318+jehunseo@users.noreply.github.com>
2020-12-30 22:52:36 +0900

    Create 2742.c

---
+++ Baekjoon/2742.c
@@ -0,0 +1,15 @@
+//link : https://www.acmicpc.net/problem/2742
+#include <stdio.h>
+
+int main(void)
+{
+    int var1, i;
+    scanf("%d", &var1);
+    
+    for(i=var1;i>0;i--)
+    {
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}
