6f711cc2f02e2a62701754fb87efc8e1c5ebf3ab
jehunseo <31786318+jehunseo@users.noreply.github.com>
2020-12-30 22:37:23 +0900

    Update 1085.py

--- Baekjoon/1085.py
+++ Baekjoon/1085.py
@@ -1,4 +1,4 @@
-//link : https://www.acmicpc.net/problem/1085
+#link : https://www.acmicpc.net/problem/1085
 arr = [int(i) for i in input().split()]
 x = [arr[0], arr[2] - arr[0]]
 y = [arr[1], arr[3] - arr[1]]
