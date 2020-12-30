7220dd3abfd39d6594152c78ed4e0d00d1e70778
jehunseo <31786318+jehunseo@users.noreply.github.com>
2020-12-30 22:52:17 +0900

    Create 2741.c

---
+++ Baekjoon/2741.c
@@ -0,0 +1,15 @@
+//link : https://www.acmicpc.net/problem/2741
+#include <stdio.h>
+
+int main(void)
+{
+    int var1, i;
+    scanf("%d", &var1);
+    
+    for(i=1;i<=var1;i++)
+    {
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}
