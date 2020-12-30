f8830383f5cc676fdeaacd551e4fa6264895bda1
jehunseo <31786318+jehunseo@users.noreply.github.com>
2020-12-30 22:33:50 +0900

    Create 1000.c

---
+++ Baekjoon/1000.c
@@ -0,0 +1,12 @@
+//link : https://www.acmicpc.net/problem/1000
+#include <stdio.h>
+
+int main(void){
+    int var1, var2;
+   
+    scanf("%d", &var1);
+    scanf("%d", &var2);
+    printf("%d", var1+var2);
+    
+    return 0;
+}
