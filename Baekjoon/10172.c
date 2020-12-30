e373193adf004ecec2e4ad1049a16f2a90a11f11
jehunseo <31786318+jehunseo@users.noreply.github.com>
2020-12-30 22:59:54 +0900

    Create 10172.c

---
+++ Baekjoon/10172.c
@@ -0,0 +1,12 @@
+//link : https://www.acmicpc.net/problem/10172
+#include <stdio.h>
+
+int main(void){
+    printf("|\\_/|\n");
+	printf("|q p|   /}\n");
+	printf("( 0 )\"\"\"\\\n");
+	printf("|\"^\"`    |\ \n");
+	printf("||_/=\\\\__|\n");
+    
+    return 0;
+}
