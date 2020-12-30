99c354dcf27b5423c223ba59046d54345850bd26
jehunseo <31786318+jehunseo@users.noreply.github.com>
2020-12-30 22:36:51 +0900

    Create 1085.py

---
+++ Baekjoon/1085.py
@@ -0,0 +1,6 @@
+//link : https://www.acmicpc.net/problem/1085
+arr = [int(i) for i in input().split()]
+x = [arr[0], arr[2] - arr[0]]
+y = [arr[1], arr[3] - arr[1]]
+if(min(x) > min(y)) :print(min(y))
+else : print(min(x))
