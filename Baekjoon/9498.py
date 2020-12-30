7ddc97b1300818c01364704c5a1707dfc3aa1253
jehunseo <31786318+jehunseo@users.noreply.github.com>
2020-12-30 22:58:40 +0900

    Create 9498.py

---
+++ Baekjoon/9498.py
@@ -0,0 +1,13 @@
+#link : https://www.acmicpc.net/problem/9498
+A = int(input())
+    
+if A >= 90 :
+	print('A')
+elif A>=80:
+	print('B')
+elif(A>=70):
+	print('C')
+elif(A>=60):
+	print('D')
+else:
+	print('F')
