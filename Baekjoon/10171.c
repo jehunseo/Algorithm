77b7f926cf8f32c27e19557e1d7a9231ad68e597
jehunseo <31786318+jehunseo@users.noreply.github.com>
2020-12-30 22:59:37 +0900

    Create 10171.c

---
+++ Baekjoon/10171.c
@@ -0,0 +1,10 @@
+//link : https://www.acmicpc.net/problem/10171
+#include <stdio.h>
+
+int main(void) { 
+	printf("\\    /\\ \n");
+	printf(" )  ( \')\n");
+	printf("(  /  )\n");
+	printf(" \\(__)|");
+	return 0;
+}
