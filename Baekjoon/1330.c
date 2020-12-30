cdde2892ad3efac82ade91a4bf58cd05e2e9b1dc
jehunseo <31786318+jehunseo@users.noreply.github.com>
2020-12-30 22:39:45 +0900

    Create 1330.c

---
+++ Baekjoon/1330.c
@@ -0,0 +1,14 @@
+//link : https://www.acmicpc.net/problem/1330
+#include <stdio.h>
+
+int main(void)
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+    
+    if(A < B) printf("<");
+    else if(A==B) printf("==");
+    else printf(">");
+    
+    return 0;
+}
