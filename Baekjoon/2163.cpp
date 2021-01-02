b062d5bfe95f86bf2f6338774485fc78709ef807
jehunseo <31786318+jehunseo@users.noreply.github.com>
2021-01-02 21:37:24 +0900

    Create 2163.cpp

---
+++ Baekjoon/2163.cpp
@@ -0,0 +1,11 @@
+//link : https://www.acmicpc.net/problem/2163
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n, m;
+	cin >> n >> m;
+	cout << (n - 1) + n * (m - 1);
+	return 0;
+}
