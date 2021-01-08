fa89a95201b3a5606793a7b33c4dc04b6fd95ba1
jehunseo <31786318+jehunseo@users.noreply.github.com>
2021-01-08 13:19:27 +0900

    Create 1874.cpp

---
+++ Baekjoon/1874.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+	// your code goes here
+	stack<int> in;
+	stack<char> ans, ans_;
+	int a;
+	cin >> a;
+	
+	for(int i = 0, j = 0 ; i < a ; i++){
+		 int b;
+		 cin >> b;
+		 if(b > j){
+		 	while(b != j){
+		 		in.push(++j);
+		 		ans.push('+');
+		 	} 
+		 }
+		 if(b == j || b == in.top()){
+		 	in.pop();
+		 	ans.push('-');
+		 }
+		 else{
+		 	cout << "NO";
+		 	return 0;
+		 }
+	}
+	
+	
+	while(!ans.empty()){
+		ans_.push(ans.top());
+		ans.pop();
+	}
+	while(!ans_.empty()){
+		cout << ans_.top() << "\n";
+		ans_.pop();
+	}
+	return 0;
+}
