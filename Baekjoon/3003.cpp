5ceb90e4a56b207ffda53e0bb2504eafc56a1c18
jehunseo <31786318+jehunseo@users.noreply.github.com>
2021-01-08 19:40:06 +0900

    Create 3003.cpp

---
+++ Baekjoon/3003.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a, b, c, d, e, f;
+	cin >> a >> b >> c >> d >> e >> f;
+	printf("%d %d %d %d %d %d", 1-a, 1-b, 2-c, 2-d, 2-e, 8-f); 
+	return 0;
+}
