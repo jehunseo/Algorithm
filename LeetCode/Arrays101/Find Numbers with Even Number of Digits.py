21ca54562afbb3d72a64bc62bf2eb12bed48e4cc
jehunseo <31786318+jehunseo@users.noreply.github.com>
2021-01-31 10:55:15 +0900

    Delete LeetCode/Arrays101 directory

--- LeetCode/Arrays101/Find Numbers with Even Number of Digits.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def findNumbers(self, nums: List[int]) -> int:
-        ans = 0
-        for l in nums:
-            if len(str(l)) % 2 == 0:
-                ans += 1
-        return ans
