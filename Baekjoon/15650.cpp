86b417bbe488b4cebec9c57835ace9bf2d8929e8
jehunseo <31786318+jehunseo@users.noreply.github.com>
2021-02-10 13:49:15 +0900

    Create 15650.cpp

---
+++ Baekjoon/15650.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+int arr[8];
+bool used[8];
+
+void func(int k){
+	if(k > 1 && (arr[k-2] > arr[k-1])){
+		return;
+	}
+			
+	if(k == m){
+		for(int i = 0 ; i < m ; i++){
+			cout << 1 + arr[i] << ' ';
+		}
+		cout << "\n";
+		return;
+	}
+	
+	for(int i = 0 ; i < n ; i++){
+		if(!used[i]){
+			arr[k] = i;
+			
+
+			
+			used[i] = 1;
+			func(k+1);
+			used[i] = 0;
+		}
+	}
+}
+
+int main(void){
+	cin >> n >> m;
+	func(0);
+}
